baa0003af9e49c0f104dc7fc9b64ec219384c3d0
Michael Junek <michael@juneks.com.au>
2022-10-26 14:43:12 +1100

    Import Bootstrap, Popper, JQuery. Create base root controller. Create source code controller for AGPL requirement

--- src/main/java/com/slinkytoybox/gcloud/gcloudmanagement/GCloudManagementApplication.java
+++ src/main/java/com/slinkytoybox/gcloud/gcloudmanagement/GCloudManagementApplication.java
@@ -19,14 +19,46 @@
  */
 package com.slinkytoybox.gcloud.gcloudmanagement;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
+@Slf4j
 public class GCloudManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GCloudManagementApplication.class, args);
-	}
+    private static String consoleLogo;
+    private final Object logoLock = new Object();
 
+    public static void main(String[] args) {
+        final String logPrefix = "main() - ";
+        log.trace("{}Entering method", logPrefix);
+        log.info("{}Booting SpringBoot application", logPrefix);
+        SpringApplication.run(GCloudManagementApplication.class, args);
+        log.info("{}Running on JVM: {} {} version {}", logPrefix, System.getProperty("java.vm.vendor"), System.getProperty("java.vm.name"), System.getProperty("java.version"));
+        log.info(consoleLogo);
+        log.trace("{}Leaving method", logPrefix);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        log.info("Reading GIT Properties: {}", propsConfig);
+        return propsConfig;
+    }
+
+    @Value("${console.logo}")
+    public void setLogo(String consoleLogo) {
+        synchronized (logoLock) {
+            GCloudManagementApplication.consoleLogo = consoleLogo;
+        }
+
+    }
 }
